d89306073eb0c0504fc5c7817c9f9937999aa212
dzadroga <dzadroga@asu.edu>
2026-03-05 22:18:58 -0500

    Completes process packet def and adds ML prediction, prints the features

--- Network_Intrusion_Detector/sniffer.py
+++ Network_Intrusion_Detector/sniffer.py
@@ -2,7 +2,7 @@ from scapy.all import sniff, IP, TCP, UDP
 import pandas as pd
 import time
 from collections import defaultdict
-import job lib
+import joblib
 
 #Load the trained ML model
 try: 
@@ -14,4 +14,51 @@ syn_counter - defaultdict(int)
 
 data = []
 
-def process_packet(packet)
+def process_packet(packet):
+    if IP in packet: 
+        src = packet[IP].src
+        dst = packet[IP].dst
+        proto = packet[IP].proto
+        length = len(packet)
+        syn_flag = 0
+
+    if TCP in packet: 
+        flags = packet[TCP].flags
+
+        if flags == "S":
+            syn_flag = 1
+            syn_counter += 1
+
+    feature = {
+        "time":time.time(),
+        "src":src,
+        "dst":dst,
+        "proto":proto,
+        "length":length,
+        "syn_flag":syn_flag,
+        "syn_count":syn_counter[src]
+    }
+
+    #ML prediction 
+    if model:
+        df = pd.DataFrame([{
+            "protocol":proto,
+            "length":length, 
+            "syn_flag":syn_flag,
+            "syn_count":syn_counter[src]
+        }])
+
+        prediction = model.predict(df)[0]
+        feature["prediction"] = prediction
+    else:
+        feature["prediction"] = "unknown"
+
+    data.append(feature)
+
+    print(feature)
+
+    pd.DataFrame(data).to_csv("traffic_log.csv", index=False)
+
+print("Starting Packet Capture")
+
+sniff(prn = process_packet, store=False)
