4e5198aa519f73af87d080e95335bb2bfee67743
dzadroga <dzadroga@asu.edu>
2026-03-12 21:39:35 -0400

    Creates title for dashboard

--- Network_Intrusion_Detector/dashboard.py
+++ Network_Intrusion_Detector/dashboard.py
@@ -2,3 +2,10 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.title("Network Intrusion Detection Dashboard")
+
+try:
+    df = pd.read_csv("traffic_log.csv")
+except: 
+    st.warning("No traffic captured yet")
+    st.stop()
