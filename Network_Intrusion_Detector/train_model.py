9cc29681aaa0ec719a9bf6b04be305655de372f8
dzadroga <dzadroga@asu.edu>
2026-03-12 21:34:56 -0400

    Adds label meaning comment

--- Network_Intrusion_Detector/train_model.py
+++ Network_Intrusion_Detector/train_model.py
@@ -1,2 +1,23 @@
 import pandas as pd
-form sklearn.ensble
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import joblib
+
+df = pd.read_csv("traffic_log.csv")
+
+#if no label exists 
+if "label" not in df.columns:
+    df["label"] = df["syn_count"].apply(lambda x: 1 if x > 20 else 0)
+
+X = df[["protol", "length", "syn_flag", "syn_count"]]
+y = df["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+joblib.dump(model, "traffic_model.pkl")
+
+print("Model has been trained and saved.")
+
